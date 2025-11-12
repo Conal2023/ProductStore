0e0a7dddfd99b9a0367f938f4050d5491cff453e
Conal <conalmccrory@gmail.com>
2025-11-12 22:29:22 +0000

    Create Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage.jsx"
 import CreatePage from "./pages/CreatePage.jsx"
 import Navbar from "./components/Navbar.jsx"
+import { useColorModeValue } from "@chakra-ui/react"
 
 
 
@@ -10,7 +11,7 @@ import Navbar from "./components/Navbar.jsx"
 function App() {
 
   return (
-    <Box minH={'100vh'}>
+    <Box minH={'100vh'} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
